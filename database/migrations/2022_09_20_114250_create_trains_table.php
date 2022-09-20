97cd931442bc3c3f65dfc4eade2c3cb40e1d26d4
virtu-alex <alessio.cordari@gmail.com>
2022-09-20 14:51:18 +0200

    feat: Add Tables

--- database/migrations/2022_09_20_114250_create_trains_table.php
+++ database/migrations/2022_09_20_114250_create_trains_table.php
@@ -16,6 +16,15 @@ class CreateTrainsTable extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('azienda', 30);
+            $table->string('stazione_di_partenza', 30);
+            $table->string('stazione_di_arrivo', 30);
+            $table->smallInteger('orario_di_partenza');
+            $table->smallInteger('orario_di_arrivo');
+            $table->integer('codice_treno');
+            $table->integer('numero_carrozze');
+            $table->boolean('in_orario', 1);
+            $table->boolean('cancellato', 1);
         });
     }
     /**
