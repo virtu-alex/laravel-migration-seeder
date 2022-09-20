6662d86f4ee06259cb76b6683e3b8e2ac6bd0abe
virtu-alex <alessio.cordari@gmail.com>
2022-09-20 13:39:56 +0200

    feat: Base Structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('main-content')
+<h1>ciao</h1>
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,8 @@
+<header class="d-flex">
+    <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">Home</a>
+    </nav>
+    <nav class="navbar navbar-light bg-light">
+        <a href="#" class="navbar-brand mb-0 h1">Trains</a>
+    </nav>
+</header>
